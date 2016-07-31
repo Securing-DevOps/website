d48f00fdfecc778000e065b05f305136cfb9cd88
Julien Vehent <julien@linuxwall.info>
2016-07-31 18:40:58 -0400

    Add piwik tracking code

---
+++ public/js/piwik.js
@@ -0,0 +1,12 @@
+var _paq = _paq || [];
+_paq.push(["setDomains", ["*.securing-devops.com","*.jvehent.github.io/continuous-security-talk"]]);
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="//linuxwall.info/webstats/";
+  _paq.push(['setTrackerUrl', u+'piwik.php']);
+  _paq.push(['setSiteId', 5]);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+})();
+
